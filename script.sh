a8ef414fe1cec0f37747afb321952479875070d5
Anton Kreida VM <kreida.anton@vm>
2026-09-22 23:28:15 +0500

    fix: path LOG_FILE, checkind find file

--- script.sh
+++ script.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-LOG_FILE="/var/log/auth.log"
+LOG_FILE="/var/log/syslog"
+
+if [[ ! -f "$LOG_FILE" ]]; then
+	echo "File not found"
+	exit 1
+fi
 
 grep -aiE "error|fail" "$LOG_FILE" > report.txt
